0c1522a1993057719dcb44d668a2e9f9cd961108
zaeem <zaeem.kadri@gmail.com>
2025-07-10 13:25:26 +0530

    Updated Resume

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1778341570"
+    serviceWorkerVersion: "703066207"
   }
 });
