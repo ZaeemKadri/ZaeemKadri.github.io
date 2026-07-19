2dea45292027e6d80a7daf91acb2d9269eafedea
zaeem <zaeem.kadri@gmail.com>
2026-07-19 18:50:16 +0530

    Fixed company url tapping in mobile view

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1070626886"
+    serviceWorkerVersion: "2327625490"
   }
 });
